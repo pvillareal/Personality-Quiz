87c6e6bf19d67a96b91dec1e7420c074f2e06f6e
Philippe <villareal.philippe@gmail.com>
2024-04-03 11:33:30 -0700

    Page 658

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class QuestionViewController: UIViewController {
 
     
-    @IBOutlet var questionLAbel: UILabel!
+    @IBOutlet var questionLabel: UILabel!
     
     @IBOutlet var singleStackView: UIStackView!
     
@@ -67,13 +67,22 @@ class QuestionViewController: UIViewController {
         )
     ]
     
+    
+    @IBOutlet var multiSwitch1: UISwitch!
+    @IBOutlet var multiSwitch2: UISwitch!
+    @IBOutlet var multiSwitch3: UISwitch!
+    @IBOutlet var multiSwitch4: UISwitch!
+    @IBOutlet var rangedSlider: UISlider!
+    
     var questionIndex = 0
+    
+    var answersChosen: [Answer] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
-    
+
     
     func updateUI() {
         singleStackView.isHidden = true
@@ -85,7 +94,7 @@ class QuestionViewController: UIViewController {
         let totalProgress = Float(questionIndex) / Float(questions.count)
         
         navigationItem.title = "Question #\(questionIndex + 1)"
-        questionLAbel.text = currentQuestion.text
+        questionLabel.text = currentQuestion.text
         questionProgressView.setProgress(totalProgress, animated: true)
         
         switch currentQuestion.type {
@@ -108,6 +117,10 @@ class QuestionViewController: UIViewController {
     
     func updateMultipleStack(using answers: [Answer]) {
         multipleStackView.isHidden = false
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiSwitch3.isOn = false
+        multiSwitch4.isOn = false
         multiLabel1.text = answers[0].text
         multiLabel2.text = answers[1].text
         multiLabel3.text = answers[2].text
@@ -116,8 +129,73 @@ class QuestionViewController: UIViewController {
     
     func updateRangeStack(using answers: [Answer]) {
         rangedStackView.isHidden = false
+        rangedSlider.setValue(0.5, animated: false)
         rangedLabel1.text = answers.first?.text
         rangedLabel2.text = answers.last?.text
     }
-
+    
+    @IBSegueAction func showResults(_ coder: NSCoder) -> ResultsViewController? {
+        return ResultsViewController(coder: coder, responses: answersChosen)
+    }
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        nextQuestion()
+    }
+    
+    func nextQuestion() -> Void {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "Results", sender: nil)
+        }
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
+    }
+    
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        
+        answersChosen.append(currentAnswers[index])
+        
+        nextQuestion()
+    }
+    
+    
 }
--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    var responses: [Answer]
+    
+    @IBOutlet var resultsAnswerLabel: UILabel!
+    @IBOutlet var resultsDefinitionLabel: UILabel!
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+        navigationItem.hidesBackButton = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [:]) {
+            (counts, answer) in counts[answer.type, default: 0] += 1
+        }
+        
+        let frequencyOfAnswersSorted = frequencyOfAnswers.sorted(by: {
+            (pair1, pair2) in return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequencyOfAnswersSorted.first!.key
+        
+        resultsAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultsDefinitionLabel.text = mostCommonAnswer.definition
+    }
+    
     
 
     /*
